707904e95bc8b94c68d49bde45666aa7b3c999d2
SpikyStars <arechestnutsspiky@gmail.com>
2022-12-05 23:59:11 -0800

    Day 6 puzzle 2

--- src/day6/Solution.java
+++ src/day6/Solution.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Solution {
     private static final boolean testMode = false;
-    private static final int puzzleNum = 1;
+    private static final int puzzleNum = 2;
 
     public static void main(String[] args) {
         String fileName = testMode ? "testInput.txt" : "input.txt";
@@ -15,7 +15,7 @@ public class Solution {
 
         switch (puzzleNum) {
             case 1 -> puzzle1(filePath);
-//            case 2 -> puzzle2(filePath);
+            case 2 -> puzzle2(filePath);
         }
     }
 
@@ -39,5 +39,40 @@ public class Solution {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void puzzle2(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line = br.readLine();
+
+            int idx = 0;
+
+            System.out.println(puzzle2Helper(line, idx) + 1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int puzzle2Helper (String line, int idx) {
+        int result = Integer.MAX_VALUE;
+        if (idx >= line.length()) {
+            return Integer.MAX_VALUE;
+        } else {
+            Set<Character> uniqueChars = new HashSet<>();
+            for (int i = idx; i < line.length(); i++) {
+                char curChar = line.charAt(i);
+                if (uniqueChars.add(curChar)) {
+                    if (uniqueChars.size() == 14) {
+                        result = i;
+//                        System.out.println("Found solution " + uniqueChars + " at index " + i);
+                        break;
+                    }
+                } else {
+                    uniqueChars.clear();
+                }
+            }
+        }
+        return Math.min(result, puzzle2Helper(line, idx + 1));
     }
 }
