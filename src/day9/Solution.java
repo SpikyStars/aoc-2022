42dad501d30c542ddb7a7b9720e8d27635edd8fc
SpikyStars <arechestnutsspiky@gmail.com>
2022-12-13 23:55:18 -0800

    Day 9 part 2

--- src/day9/Solution.java
+++ src/day9/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 
     public static void main(String[] args) {
         String fileName = testMode ? "testInput.txt" : "input.txt";
-        fileName = "testInput2.txt";
+//        fileName = "testInput2.txt";
         String filePath = "src/day9/" + fileName;
 
         switch (puzzleNum) {
@@ -112,7 +112,11 @@ public class Solution {
 //            }
 //            System.out.println(test);
 
-
+            // debugging
+            int largestX = 0;
+            int largestY = 0;
+            int smallestX = 0;
+            int smallestY = 0;
 
             Set<IntPair> tailLocations = new HashSet<>();
 
@@ -126,14 +130,27 @@ public class Solution {
                 int steps = Integer.parseInt(instr[1]);
                 for (int i = steps; i > 0; i--) {
                     switch (instr[0]) {
-                        case "R" -> head.loc.first++;
-                        case "L" -> head.loc.first--;
-                        case "U" -> head.loc.second++;
-                        case "D" -> head.loc.second--;
+                        case "R" -> {
+                            head.loc.first++;
+                            if (head.loc.first > largestX) largestX = head.loc.first;
+                        }
+                        case "L" -> {
+                            head.loc.first--;
+                            if (head.loc.first < smallestX) smallestX = head.loc.first;
+                        }
+                        case "U" -> {
+                            head.loc.second++;
+                            if (head.loc.second > largestY) largestY = head.loc.second;
+                        }
+                        case "D" -> {
+                            head.loc.second--;
+                            if (head.loc.second < smallestY) smallestY = head.loc.second;
+                        }
                     }
                     Knot prevKnot = head;
                     curKnot = head.next; // thinking of new variable names is too hard
-                    while (curKnot != null) {
+                    while (curKnot != null) { // New case? H (2, 2) and T (0, 0) -> T (1, 1) not T (1, 2)
+                        // H (0, 1) and T(2, 1) -> T (1, 1) not T(2, 2)
                         if (Math.abs(prevKnot.loc.first - curKnot.loc.first) == 2 || Math.abs(prevKnot.loc.second - curKnot.loc.second) == 2) {
                             // Case where they are not touching and not in the same row or column
                             if (prevKnot.loc.first != curKnot.loc.first && prevKnot.loc.second != curKnot.loc.second) {
@@ -141,8 +158,11 @@ public class Solution {
                                 // H (-3, 2) and T (-5, 1) -> T (-4, 2)
                                 // H (3, 2) and T (5, 1) -> T (4, 2)
                                 // H (3, 2) and T (1, 1) -> T (2, 2)
-                                // Head is farther away on the x-axis
-                                if (Math.abs(prevKnot.loc.first - curKnot.loc.first) == 2) {
+                                if (Math.abs(prevKnot.loc.first - curKnot.loc.first) == 2 && Math.abs(prevKnot.loc.second - curKnot.loc.second) == 2) {
+                                    curKnot.loc.first += Integer.signum(prevKnot.loc.first - curKnot.loc.first);
+                                    curKnot.loc.second += Integer.signum(prevKnot.loc.second - curKnot.loc.second);
+                                }
+                                else if (Math.abs(prevKnot.loc.first - curKnot.loc.first) == 2) {
                                     curKnot.loc.first += Integer.signum(prevKnot.loc.first - curKnot.loc.first);
                                     curKnot.loc.second = prevKnot.loc.second;
                                 } else {
@@ -153,30 +173,51 @@ public class Solution {
                                 }
                             } else {
                                 // Case where head is directly 2 steps from the tail
-                                switch (instr[0]) {
-                                    case "R" -> curKnot.loc.first++;
-                                    case "L" -> curKnot.loc.first--;
-                                    case "U" -> curKnot.loc.second++;
-                                    case "D" -> curKnot.loc.second--;
+
+                                // The logic can absolutely be simplified given what's above.
+                                // Am I going to touch this mess now that I spent a couple hours just to debug and solve this? no
+                                if (prevKnot.loc.first == curKnot.loc.first) {
+                                    curKnot.loc.second += Integer.signum(prevKnot.loc.second - curKnot.loc.second);
+                                } else {
+                                    curKnot.loc.first += Integer.signum(prevKnot.loc.first - curKnot.loc.first);
                                 }
                             }
                         }
                         // At the tail
                         if (curKnot.next == null) {
-                              System.out.println("Head: " + head.loc.first + ", " + head.loc.second);
-                              System.out.println("Tail: " + curKnot.loc.first + ", " + curKnot.loc.second);
                             IntPair curTailLoc = new IntPair(curKnot.loc.first, curKnot.loc.second);
                             tailLocations.add(curTailLoc);
                         }
                         prevKnot = curKnot;
                         curKnot = curKnot.next;
                     }
+
+//                    // DEBUGGING PRINT
+//                    StringBuilder row = new StringBuilder();
+//                    for (int p = largestY; p >= smallestY; p--) {
+//                        for (int j = smallestX; j <= largestX; j++) {
+//                            IntPair curCoords = new IntPair(j, p);
+//                            Knot debug = head;
+//                            String res = ".";
+//                            if (debug.loc.equals(curCoords)) {
+//                                res = "H";
+//                            }
+//                            for (int q = 0; q < 9; q++) {
+//                                if (debug.next.loc.equals(curCoords)) {
+//                                    res = Integer.toString(q + 1);
+//                                    break;
+//                                }
+//                                debug = debug.next;
+//                            }
+//                            row.append(res);
+//                        }
+//                        row.append("\n");
+//                    }
+//                    row.append("\n");
+//                    System.out.print(row);
                 }
                 line = br.readLine();
             }
-//            for (IntPair i : tailLocations.stream().toList()) {
-//                System.out.println(i.first + ", " + i.second);
-//            }
             System.out.println(tailLocations.size());
 
         } catch (IOException e) {
